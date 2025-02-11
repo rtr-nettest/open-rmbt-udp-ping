a8d5d880084bc52f1c3f781dc66d42b67dc3c683
Dietmar Zlabinger <dietmar@zlabinger.at>
2025-02-12 00:37:27 +0100

    Remove incomplete xdp

--- rust-server-xdp/xdp-prog/build.rs
+++
@@ -1,9 +0,0 @@
-fn main() {
-    cc::Build::new()
-        .file("src/xdp_kern.c")
-        .flag("-target")
-        .flag("bpf")
-        .flag("-O2")
-        .flag("-g")
-        .compile("xdp_prog");
-}
